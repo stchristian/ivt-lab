8e3bfe96d8cb7cd698681d12668a7df5431a0b0f
Hübner Krisztián <hubner.krisztian97@gmail.com>
2019-05-10 12:47:15 +0200

    Mockolt torpedostoreok a teszteknél

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -7,34 +7,49 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
-
+  private TorpedoStore mockPTS;
+  private TorpedoStore mockSTS;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPTS = mock(TorpedoStore.class);
+    mockSTS = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPTS, mockSTS);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockPTS.isEmpty()).thenReturn(false);
+    when(mockPTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(mockPTS, times(1)).isEmpty();
+    verify(mockPTS, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockPTS.isEmpty()).thenReturn(false);
+    when(mockPTS.fire(1)).thenReturn(true);
+    when(mockSTS.isEmpty()).thenReturn(false);
+    when(mockSTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(mockPTS, times(1)).isEmpty();
+    verify(mockPTS, times(1)).fire(1);
+    verify(mockSTS, times(1)).isEmpty();
+    verify(mockSTS, times(1)).fire(1);
   }
 
 }
